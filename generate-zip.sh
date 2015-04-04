e4498f82555b742f489e0daeb973325834f41369
Daniel Vijge <danielvijge@users.noreply.github.com>
2015-04-04 19:57:16 +0200

    Correct URL for GitHub pages in public.xml

--- generate-zip.sh
+++ generate-zip.sh
@@ -15,8 +15,8 @@ cat <<EOF > public.xml
 		<plugin name="YouTube" version="$VERSION" minTarget="7.5" maxTarget="*">
 			<title lang="EN">YouTube</title>
 			<desc lang="EN">Browse, search and play urls from soundcloud</desc>
-			<url>http://danielvijge.github.io/YouTube/YouTube-$VERSION.zip</url>
-			<link>https://github.com/danielvijge/YouTube</link>
+			<url>http://danielvijge.github.io/squeezebox-youtube/YouTube-$VERSION.zip</url>
+			<link>https://github.com/danielvijge/squeezebox-youtube</link>
 			<sha>$SHA</sha>
 			<creator>Triode, murkle, djbloc, Daniel Vijge, Daniel P. Berrange</creator>
 		</plugin>
