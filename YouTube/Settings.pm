37172af169356d178dac74383daf4c9459950ec7
Daniel Vijge <danielvijge@users.noreply.github.com>
2017-02-05 21:03:15 +0100

    Version 0.50.0

--- YouTube/Settings.pm
+++ YouTube/Settings.pm
@@ -10,6 +10,7 @@ use Slim::Utils::Prefs;
 use Slim::Utils::Log;
 
 my $log   = logger('plugin.youtube');
+my $cache = Slim::Utils::Cache->new();
 
 sub name {
 	return 'PLUGIN_YOUTUBE';
@@ -20,7 +21,7 @@ sub page {
 }
 
 sub prefs {
-	return (preferences('plugin.youtube'), qw(channel_prefix channel_suffix playlist_prefix playlist_suffix country max_items APIkey));
+	return (preferences('plugin.youtube'), qw(channel_prefix channel_suffix playlist_prefix playlist_suffix country max_items APIkey client_id client_secret));
 }
 
 sub handler {
@@ -32,10 +33,20 @@ sub handler {
 		Plugins::YouTube::API::flushCache();
 		Plugins::YouTube::ProtocolHandler::flushCache();
 	}
-=cut	
+=cut
 
-	$params->{pref_max_items} = min($params->{pref_max_items}, 500);
+	Plugins::YouTube::Oauth2::getCode if $params->{get_code};
 	
+	$params->{user_code} = $cache->get('yt:user_code');
+	$params->{verification_url} = $cache->get('yt:verification_url');
+	$params->{access_code} = $cache->get('yt:access_code');
+	$params->{authorize_link} = $cache->get('yt:verification_url');
+	$params->{access_token} = $cache->get('yt:access_token');
+	
+	$params->{pref_max_items} = min($params->{pref_max_items}, 500);
+		
+	$cache->remove('yt:access_token') if $params->{clear_token};
+		
 	$callback->($client, $params, $class->SUPER::handler($client, $params), @args);
 }
 
